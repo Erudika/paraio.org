6d0140127741d8a67d4a363b83a48161beb9b412
Alex Bogdanovski <alex@erudika.com>
2014-06-18 19:29:17 +0300

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if [ -n "$1" ]; then
-  grunt && git commit -am "updated docs" && git checkout gh-pages
-  cp -Rf dist/* .
+
+grunt && git commit -am "updated docs" && git checkout gh-pages
+cp -Rf dist/* .
+if [ -n "$1" ]; then  
   rm -rf javadocs
   wget -O jdocs.zip https://oss.sonatype.org/service/local/repositories/releases/content/com/erudika/para/$1/para-$1-javadoc.jar && 
   unzip jdocs.zip -d javadocs && rm jdocs.zip
-  git commit -am "updated docs"
-  git push origin gh-pages
-  git checkout master
-  echo "done"
 fi
+git add -A && git commit -am "updated docs"
+git push origin gh-pages
+git checkout master
+echo "-- done --"
