fb943d745c21dda03f551bd8cbb3b04433370005
Alex Bogdanovski <alex@erudika.com>
2015-04-05 20:47:26 +0300

    updated docs

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ grunt && git commit -am "updated docs" && git checkout gh-pages
 cp -Rf dist/* .
 if [ -n "$1" ]; then  
   rm -rf javadocs
-  wget -O jdocs.zip https://oss.sonatype.org/service/local/repositories/releases/content/com/erudika/para/$1/para-$1-javadoc.jar && 
+  wget -O jdocs.zip https://oss.sonatype.org/service/local/repositories/releases/content/com/erudika/para-core/$1/para-core-$1-javadoc.jar && 
   unzip jdocs.zip -d javadocs && rm jdocs.zip
 fi
 git add -A && git commit -am "updated docs"
