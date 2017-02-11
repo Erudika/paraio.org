26e02e8e50982a607f46de1d3f786ac95eac3de4
Alex Bogdanovski <alex@erudika.com>
2017-02-11 14:25:45 +0200

    updated docs

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 DIST="docs"
-rm -rf $DIST
-grunt cb
 if [ -n "$1" ]; then
   sed -e "1,/version: / s/version: .*/version: \"$1\"/" -i.bak ./src/monolite/index.html
 	rm jdocs1.zip jdocs2.zip jdocs3.zip ./src/monolite/index.html.bak
@@ -10,6 +8,7 @@ if [ -n "$1" ]; then
   wget -O jdocs2.zip https://oss.sonatype.org/service/local/repositories/releases/content/com/erudika/para-server/$1/para-server-$1-javadoc.jar &&
   wget -O jdocs3.zip https://oss.sonatype.org/service/local/repositories/releases/content/com/erudika/para-client/$1/para-client-$1-javadoc.jar
 fi
+grunt cb
 unzip jdocs1.zip -d ${DIST}/javadocs-core
 unzip jdocs2.zip -d ${DIST}/javadocs-server
 unzip jdocs3.zip -d ${DIST}/javadocs-client
