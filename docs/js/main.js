bc8973d9810f4153e458849c336736192ca86524
Alex Bogdanovski <alex@erudika.com>
2017-05-23 00:27:49 +0300

    updated docs

--- docs/js/main.js
+++ docs/js/main.js
@@ -65,3 +65,40 @@ $("#sidebar a").each(function () {
 
 // Making our navigation sticky
 new Filter($("#filter-form"), $("#sidebar > ul"));
+
+/// TEXT ROTATOR
+$.fn.extend({
+	rotaterator: function (options) {
+		var defaults = {
+			fadeSpeed: 500,
+			pauseSpeed: 1000,
+			child: null
+		};
+		var options = $.extend(defaults, options);
+
+		return this.each(function () {
+			var o = options;
+			var obj = $(this);
+			var items = $(obj.children(), obj);
+			items.each(function () {
+				$(this).hide();
+			});
+			if (!o.child) {
+				var next = $(obj).children(':first');
+			} else {
+				var next = o.child;
+			}
+			$(next).fadeIn(o.fadeSpeed, function () {
+				$(next).delay(o.pauseSpeed).fadeOut(o.fadeSpeed, function () {
+					var next = $(this).next();
+					if (next.length === 0) {
+						next = $(obj).children(':first');
+					}
+					$(obj).rotaterator({child: next, fadeSpeed: o.fadeSpeed, pauseSpeed: o.pauseSpeed});
+				});
+			});
+		});
+	}
+});
+
+$(".intro-text").rotaterator({fadeSpeed: 150, pauseSpeed: 2000});
--- src/monolite/partials/001-intro.md
+++ src/monolite/partials/001-intro.md
@@ -20,7 +20,7 @@ your project. You can still keep the REST API or turn it off completely.
 ### Quick start
 
 1. [Download the WAR](https://github.com/erudika/para/releases)
-2. Run it with `java -jar para-{VERSION}.war`
+2. Run it with `java -jar para-*.war`
 3. Call `curl localhost:8080/v1/_setup` to get the access and secret keys
 4. Open the [Para Web Console](https://console.paraio.org) or use one of the provided
 client libraries below to connect to the API.
@@ -44,6 +44,8 @@ pc.setEndpoint(paraServerURL);
 // send a test request - this should return a JSON object of type 'app'
 pc.me();
 ```
+
+> We've built a full-blown StackOverflow clone with Para in just about 4000 lines of code - check it out at https://scoold.com
 
 ### Client libraries
 
