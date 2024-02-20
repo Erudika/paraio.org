9b5aab5edf17af4e35b9894e6d00aedf828236f8
Alex Bogdanovski <alex@erudika.com>
2024-02-20 23:04:12 +0200

    updated docs

--- docs/javadocs-client/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.erudika.para.client","l":"com.erudika.para.client"}];updateSearchResults();
--- docs/javadocs-client/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.erudika.para.client","l":"ParaClient"}];updateSearchResults();
