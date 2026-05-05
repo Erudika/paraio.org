aa859674e13edf15156a8faf99ee013ed9a57102
Alex Bogdanovski <alex@erudika.com>
2026-05-05 11:00:16 +0300

    updated docs

--- docs/javadocs-client/type-search-index.js
+++ docs/javadocs-client/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.erudika.para.client","l":"ParaClient"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"com.erudika.para.client","l":"ParaClient"}];updateSearchResults();
